b03a4073e247c29d1756e994bc751aa8ba2f7096
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-12 17:52:03 -0800

    changed the way error message shown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod result;
 mod state;
 mod args;
@@ -6,12 +8,10 @@ mod ui;
 use state::AppState;
 use clap::Parser;
 use eframe::egui;
-use ui::draw_ui;
-use std::thread;
 
 impl eframe::App for AppState {
   fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-    draw_ui(self, ctx);
+    ui::draw_ui(self, ctx);
   }
 
   fn on_exit(&mut self, _: Option<&eframe::glow::Context>) {
@@ -19,25 +19,29 @@ impl eframe::App for AppState {
   }
 }
 
-fn main() -> eframe::Result {
+fn main() -> result::Result {
   let args = args::Args::parse();
   let mut app_state = AppState::load().unwrap_or_default();
 
   if let Some(token) = args.token {
     app_state.openai_token = token;
   } else if app_state.openai_token.is_empty() {
+    #[cfg(not(target_os = "windows"))]
     eprintln!("OpenAI API token is required on first run!");
 
-    thread::park();
-  }
+    #[cfg(target_os = "windows")]
+    std::process::Command::new("cmd")
+      .args(["/C", "echo OpenAI API token is required on first run!", "&&", "pause"])
+      .spawn()?;
 
-  // hide console window
-  #[cfg(target_os = "windows")]
-  unsafe { winapi::um::wincon::FreeConsole() };
+    return Ok(());
+  }
 
   eframe::run_native(
     "Robo AI Chat",
     eframe::NativeOptions::default(),
     Box::new(|_| Ok(Box::new(app_state))),
-  )
+  )?;
+
+  Ok(())
 }
