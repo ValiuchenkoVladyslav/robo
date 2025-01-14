cc7aabc37e2ca5edeffe9609755f14a667b15342
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-14 09:43:22 -0800

    color fix

--- src/ui/utils.rs
+++ src/ui/utils.rs
@@ -1,9 +1,9 @@
 use eframe::egui::{Color32, Rounding, Ui};
 
-pub const DARKER: Color32 = Color32::from_rgba_premultiplied(0, 0, 0, 64);
+pub const DARKER: Color32 = Color32::from_rgb(16, 16, 16);
 
 pub fn set_input_rounding(ui: &mut Ui) {
-  const INPUT_ROUNDING: Rounding = Rounding::same(8.0);
+  const INPUT_ROUNDING: Rounding = Rounding::same(8.);
 
   let widgets = &mut ui.visuals_mut().widgets;
   widgets.inactive.rounding = INPUT_ROUNDING;
