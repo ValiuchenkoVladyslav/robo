6dfef0d987c627fa660d737254cc840f2dd179f7
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-16 13:10:02 -0800

    added api related args

--- src/args.rs
+++ src/args.rs
@@ -5,4 +5,14 @@ pub struct Args {
   /// Custom Ollama API URL. Default is http://localhost:11434
   #[arg(short, long)]
   pub ollama_url: Option<String>,
+
+  /// API only mode
+  #[cfg(all(feature = "api", feature = "gui"))]
+  #[arg(short, long, default_value = "false")]
+  pub api_mode: bool,
+
+  /// API port. Only applicable in API mode
+  #[cfg(feature = "api")]
+  #[arg(short, long, default_value = "3000")]
+  pub port: u16,
 }
