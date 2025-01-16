00ee5bd67e903b02cf19b0454b088c04d87c4bcb
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-16 12:58:47 -0800

    updated init logic

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,51 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(feature = "api")]
+mod api;
+mod args;
+#[cfg(feature = "gui")]
+mod gui;
+mod ollama;
 mod result;
 mod state;
-mod args;
-mod ui;
 
-use state::AppState;
 use clap::Parser;
-use eframe::egui;
+use state::AppState;
 
-impl eframe::App for AppState {
-  fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-    ui::draw_ui(self, ctx);
-  }
-
-  fn on_exit(&mut self, _: Option<&eframe::glow::Context>) {
-    self._save().expect("Unable to save app state");      
-  }
-}
-
-fn main() -> result::Result {
-  // create runtime for async tasks
-  let rt = tokio::runtime::Runtime::new()?;
-  let _enter = rt.enter();
-
-  // parse args and load app state
+#[tokio::main]
+async fn main() -> result::Result {
   let args = args::Args::parse();
+
   let app_state = AppState::load(args.ollama_url)?;
 
-  eframe::run_native(
-    "Robo AI Chat",
-    eframe::NativeOptions::default(),
-    Box::new(|_| Ok(Box::new(app_state))),
-  )?;
+  // refetch models without blocking main thread
+  tokio::spawn({
+    let ollama = app_state.ollama;
+    let models = app_state.models.clone();
+
+    async move {
+      if let Err(err) = ollama::list_models(ollama, models).await {
+        eprintln!(
+          "Failed to list models: {}\nMake sure Ollama is running!",
+          err
+        );
+      }
+    }
+  });
+
+  // run the GUI by default or API if specified in args
+  #[cfg(all(feature = "api", feature = "gui"))]
+  if args.api_mode {
+    api::run_server(app_state, args.port).await?;
+  } else {
+    gui::run_gui(app_state)?;
+  }
+
+  #[cfg(all(feature = "api", not(feature = "gui")))]
+  api::run_server(app_state, args.port).await?;
+
+  #[cfg(all(not(feature = "api"), feature = "gui"))]
+  gui::run_gui(app_state)?;
 
   Ok(())
 }
