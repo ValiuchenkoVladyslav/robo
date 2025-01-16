a778ec3e6ea5054f4419e7000554b2a1f4f20c6d
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-16 12:57:56 -0800

    extended error enum & actix integration

--- src/result.rs
+++ src/result.rs
@@ -6,14 +6,33 @@ pub enum Error {
   #[error("Bincode error: {0}")]
   Bincode(#[from] bincode::Error),
 
-  #[error("UI error: {0}")]
-  Ui(#[from] eframe::Error),
+  #[cfg(feature = "gui")]
+  #[error("GUI error: {0}")]
+  Gui(#[from] eframe::Error),
 
   #[error("Ollama error: {0}")]
   Ollama(#[from] ollama_rs::error::OllamaError),
 
+  // only used for port parsing; see state.rs
   #[error("Failed to parse port: {0}")]
   ParsePort(#[from] std::num::ParseIntError),
+
+  #[cfg(feature = "api")]
+  #[error("Not Found")]
+  NotFound,
 }
 
-pub type Result<T=()> = core::result::Result<T, Error>;
+#[cfg(feature = "api")]
+use actix_web::http::StatusCode;
+
+#[cfg(feature = "api")]
+impl actix_web::ResponseError for Error {
+  fn status_code(&self) -> StatusCode {
+    match self {
+      Error::NotFound => StatusCode::NOT_FOUND,
+      _ => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+  }
+}
+
+pub type Result<T = ()> = core::result::Result<T, Error>;
