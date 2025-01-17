53153c84e51440d2b1bdf443ee4b1849c01b91fe
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-17 05:37:34 -0800

    improved init logic

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,28 @@
 // mod api;
-mod args;
-// mod ollama;
 mod result;
+mod db;
 mod state;
+mod schemas;
 
-use clap::Parser;
-use state::init_state;
+use state::AppState;
 use actix_web::{App, HttpServer};
+use std::env::var;
 
 #[tokio::main]
 async fn main() -> result::Result {
-  let args = args::Args::parse();
+  let ollama_url = var("OLLAMA_URL").expect("OLLAMA_URL env var");
+  let redis_url = var("REDIS_URL").expect("REDIS_URL env var");
+  let postgres_url = var("POSTGRES_URL").expect("POSTGRES_URL env");
+  let app_port: u16 = var("APP_PORT").expect("APP_PORT env var").parse()?;
 
-  init_state(
-    {
-      let port_pos = args.ollama_url.rfind(":").expect("No port found in Ollama url!");
+  println!("Initializing app state...");
+  AppState::new(ollama_url, redis_url, postgres_url).await?;
 
-      ollama_rs::Ollama::new(
-        &args.ollama_url[..port_pos],
-        args.ollama_url[port_pos + 1..].parse()?,
-      )
-    },
-    redis::Client::open(args.redis_url)?,
-  );
+  println!("Running migrations...");
+  schemas::run_migrations().await?;
 
-  HttpServer::new(move || {
+  println!("Starting web server...");
+  HttpServer::new(|| {
     App::new()
       // .service(api::routes::get_chats)
       // .service(api::routes::create_chat)
@@ -32,7 +30,7 @@ async fn main() -> result::Result {
       // .service(api::routes::send_message)
       // .service(api::routes::delete_chat)
   })
-    .bind(("127.0.0.1", args.port))?
+    .bind(("127.0.0.1", app_port))?
     .run()
     .await?;
 
--- src/state.rs
+++ src/state.rs
@@ -1,43 +1,63 @@
 use redis::Client as Redis;
-use ollama_rs::{generation::chat::ChatMessage, models::LocalModel, Ollama};
-use serde::{Deserialize, Serialize};
+use ollama_rs::Ollama;
+use sea_orm::{Database, DatabaseConnection};
 use std::sync::OnceLock;
-
-#[derive(Debug, Default, Clone, Serialize, Deserialize)]
-pub struct Chat {
-  pub title: String,
-  pub model: String,
-  pub messages: Vec<ChatMessage>,
-}
-
-impl Chat {
-  pub fn new(models: &Vec<LocalModel>) -> Self {
-    Self {
-      title: "New Chat".to_string(),
-      model: models
-        .first()
-        .map_or("phi3.5", |model| &model.name)
-        .to_string(),
-      messages: vec![ChatMessage::system(
-        "Reply shortly, no more than asked".to_string(),
-      )],
-    }
-  }
-}
-
-struct AppState {
-  pub ollama: Ollama,
-  pub redis: Redis,
-}
+use crate::result::Result;
 
 static APP_STATE: OnceLock<AppState> = OnceLock::new();
 
-pub fn init_state(ollama: Ollama, redis: Redis) {
-  if APP_STATE.set(AppState { ollama, redis }).is_err() {
-    panic!("Failed to init app state!");
-  }
+#[derive(Debug)]
+pub struct AppState {
+  /// Ollama connection
+  pub ollama: Ollama,
+
+  /// Redis connection
+  pub redis: Redis,
+
+  /// Postgres connection pool
+  pub postgres: DatabaseConnection,
 }
 
-pub fn state() -> &'static AppState {
-  APP_STATE.get().expect("For some magical reason, the app state is not initialized!")
+impl AppState {
+  /// Initialize the app state
+  /// 
+  /// - Creates a new **Ollama** connection, **Redis** connection, and **Postgres** connection pool
+  /// 
+  /// - Puts everything into the `APP_STATE` static variable
+  /// 
+  /// Connections can be accessed via [AppState::get] function
+  pub async fn new(ollama_url: String, redis_url: String, postgres_url: String) -> Result {
+    let state = AppState {
+      ollama: {
+        let port_pos = ollama_url.rfind(':').expect("OLLAMA_URL must contain a port!");
+  
+        Ollama::new(
+          &ollama_url[..port_pos],
+          ollama_url[port_pos + 1..].parse()?,
+        )
+      },
+      redis: Redis::open(redis_url)?,
+      postgres: Database::connect(postgres_url).await?,
+    };
+
+    APP_STATE.set(state).unwrap();
+
+    Ok(())
+  }
+
+  fn get() -> &'static AppState {
+    APP_STATE.get().expect("For some magical reason, the app state is not initialized!")
+  }
+
+  pub fn db() -> &'static DatabaseConnection {
+    &AppState::get().postgres
+  }
+
+  pub fn ollama() -> &'static Ollama {
+    &AppState::get().ollama
+  }
+
+  pub fn redis() -> &'static Redis {
+    &AppState::get().redis
+  }
 }
