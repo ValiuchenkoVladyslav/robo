9e7f2b8f1ca8731e86c714d760ef98782b8f2b85
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-18 04:37:04 -0800

    applied fmt suggestions

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,8 @@
-use redis::Client as Redis;
-use ollama_rs::Ollama;
-use sea_orm::{Database, DatabaseConnection};
-use std::sync::OnceLock;
 use crate::result::Result;
+use ollama_rs::Ollama;
+use redis::Client as Redis;
+use sqlx::PgPool;
+use std::sync::OnceLock;
 
 static APP_STATE: OnceLock<AppState> = OnceLock::new();
 
@@ -11,33 +11,33 @@ pub struct AppState {
   /// Ollama connection
   pub ollama: Ollama,
 
+  #[allow(dead_code)]
   /// Redis connection
   pub redis: Redis,
 
   /// Postgres connection pool
-  pub postgres: DatabaseConnection,
+  pub postgres: PgPool,
 }
 
 impl AppState {
   /// Initialize the app state
-  /// 
+  ///
   /// - Creates a new **Ollama** connection, **Redis** connection, and **Postgres** connection pool
-  /// 
-  /// - Puts everything into the `APP_STATE` static variable
-  /// 
-  /// Connections can be accessed via [AppState::get] function
-  pub async fn new(ollama_url: String, redis_url: String, postgres_url: String) -> Result {
+  ///
+  /// - Puts everything into `APP_STATE` static variable
+  ///
+  /// Connections can be accessed via `AppState::get`
+  pub async fn init(ollama_url: String, redis_url: String, postgres_url: String) -> Result {
     let state = AppState {
       ollama: {
-        let port_pos = ollama_url.rfind(':').expect("OLLAMA_URL must contain a port!");
-  
-        Ollama::new(
-          &ollama_url[..port_pos],
-          ollama_url[port_pos + 1..].parse()?,
-        )
+        let port_pos = ollama_url
+          .rfind(':')
+          .expect("OLLAMA_URL must contain a port!");
+
+        Ollama::new(&ollama_url[..port_pos], ollama_url[port_pos + 1..].parse()?)
       },
       redis: Redis::open(redis_url)?,
-      postgres: Database::connect(postgres_url).await?,
+      postgres: PgPool::connect(&postgres_url).await?,
     };
 
     APP_STATE.set(state).unwrap();
@@ -46,10 +46,12 @@ impl AppState {
   }
 
   fn get() -> &'static AppState {
-    APP_STATE.get().expect("For some magical reason, the app state is not initialized!")
+    APP_STATE
+      .get()
+      .expect("For some magical reason, the app state is not initialized!")
   }
 
-  pub fn db() -> &'static DatabaseConnection {
+  pub fn db() -> &'static PgPool {
     &AppState::get().postgres
   }
 
@@ -57,6 +59,7 @@ impl AppState {
     &AppState::get().ollama
   }
 
+  #[allow(dead_code)]
   pub fn redis() -> &'static Redis {
     &AppState::get().redis
   }
